e21bf04b04a08f2853f41ffe7f50583ada6fe83f
whr2349 <378237242@QQ.com>
2023-08-21 16:41:50 +0800

    refactor

--- packages/vital-admin/src/App.vue
+++ packages/vital-admin/src/App.vue
@@ -3,7 +3,27 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
+     <n-space>
+    <n-button>Default</n-button>
+    <n-button type="tertiary">
+      Tertiary
+    </n-button>
+    <n-button type="primary">
+      Primary
+    </n-button>
+    <n-button type="info">
+      Info
+    </n-button>
+    <n-button type="success">
+      Success
+    </n-button>
+    <n-button type="warning">
+      Warning
+    </n-button>
+    <n-button type="error">
+      Error
+    </n-button>
+  </n-space>
     <div class="wrapper">
       <RouterView />
     </div>
--- packages/vital-admin/vite.config.ts
+++ packages/vital-admin/vite.config.ts
@@ -1,14 +1,42 @@
+/*
+ * @Author: whr2349 378237242@QQ.com
+ * @Date: 2023-08-18 16:57:42
+ * @LastEditors: whr2349 378237242@QQ.com
+ * @LastEditTime: 2023-08-21 16:23:46
+ * @FilePath: \vital-admin\packages\vital-admin\vite.config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
   ],
   resolve: {
     alias: {
