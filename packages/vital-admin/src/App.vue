aaf0faff795f4fe37b9cdc3409c638ae15e9d8c2
whr2349 <378237242@QQ.com>
2023-08-23 17:34:57 +0800

    -

--- packages/vital-admin/src/App.vue
+++ packages/vital-admin/src/App.vue
@@ -1,30 +1,45 @@
+<!--
+ * @Author: whr2349 378237242@QQ.com
+ * @Date: 2023-08-18 16:57:42
+ * @LastEditors: whr2349 378237242@QQ.com
+ * @LastEditTime: 2023-08-23 16:50:23
+ * @FilePath: \vital-admin\packages\vital-admin\src\App.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+-->
 <template>
-  <n-layout has-sider>
-    <n-layout-sider
-      bordered
-      show-trigger
-      collapse-mode="width"
-      :collapsed-width="64"
-      :width="240"
-      :native-scrollbar="false"
-    >
-      <n-layout>
-        <n-layout-header bordered><SystemLogo></SystemLogo></n-layout-header>
-        <n-layout-content bordered>
-          <n-menu :collapsed-width="64" :collapsed-icon-size="20" :options="menuOptions" />
-        </n-layout-content>
-      </n-layout>
-    </n-layout-sider>
-    <n-layout>
-      <n-layout-header bordered>颐和园路</n-layout-header>
-      <n-layout-content bordered > 平山道 </n-layout-content>
-      <n-layout-footer bordered>成府路</n-layout-footer>
+  <n-config-provider>
+    <n-layout content-style="height:100vh;">
+      <n-layout-header bordered>
+        <div class=""><SystemLogo :show-title="collapsed"></SystemLogo></div>
+      </n-layout-header>
+      <n-layout-content bordered class="">
+        <n-layout has-sider content-style="height: 800px;">
+          <n-layout-sider
+            bordered
+            :collapsed="collapsed"
+            show-trigger
+            collapse-mode="width"
+            :collapsed-width="64"
+            :width="240"
+            :native-scrollbar="false"
+            @update:collapsed="siderUpdate"
+          >
+            <n-menu :collapsed-width="64" :collapsed-icon-size="20" :options="menuOptions" />
+          </n-layout-sider>
+          <n-layout>
+            <n-layout-content content-style="padding: 24px;"> 平山道 </n-layout-content>
+            <n-layout-footer>成府路</n-layout-footer>
+          </n-layout>
+        </n-layout>
+      </n-layout-content>
     </n-layout>
-  </n-layout>
+  </n-config-provider>
 </template>
 
 <script lang="ts" setup>
-import { h, defineComponent, type Component } from 'vue'
+import { h, type Component, ref } from 'vue'
 import { NIcon } from 'naive-ui'
 import {
   BookOutline as BookIcon,
@@ -36,7 +51,297 @@ function renderIcon(icon: Component) {
   return () => h(NIcon, null, { default: () => h(icon) })
 }
 
+const collapsed = ref(false)
 const menuOptions = [
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
+  {
+    label: '且听风吟',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+  {
+    label: '1973年的弹珠玩具',
+    key: 'pinball-1973',
+    icon: renderIcon(PersonIcon),
+    children: [
+      {
+        label: '鼠',
+        key: 'rat',
+        icon: renderIcon(WineIcon)
+      }
+    ]
+  },
   {
     label: '且听风吟',
     key: 'hear-the-wind-sing',
@@ -55,10 +360,8 @@ const menuOptions = [
     ]
   }
 ]
-const sysTitle = [
-  {
-    label: 'vital-admin',
-    icon: renderIcon(BookIcon)
-  }
-]
+const siderUpdate = (value: boolean) => {
+  console.log(value)
+  collapsed.value = value
+}
 </script>
