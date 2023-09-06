1b2fee658407e3b61e25a736df5d76cbf1c1f0ca
whr2349 <378237242@QQ.com>
2023-09-06 11:16:10 +0800

    -

--- packages/components/src/components/layout/BaseLayout.vue
+++ packages/components/src/components/layout/BaseLayout.vue
@@ -2,31 +2,63 @@
  * @Author: whr2349 378237242@QQ.com
  * @Date: 2023-09-04 14:29:11
  * @LastEditors: whr2349 378237242@QQ.com
- * @LastEditTime: 2023-09-05 09:49:27
- * @FilePath: \vital-admin\packages\components\src\components\layout\BaseLayout.vue
+ * @LastEditTime: 2023-09-06 10:50:59
+ * @FilePath: \components\src\components\layout\BaseLayout.vue
  * @Description: 
  * 
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
 -->
 <template>
-  <div class="h-full bg-slate-500 flex">
-    <div class="bg-slate-400 flex flex-col relative" :style="sidebarStyle">
-      <div class="bg-slate-300" :style="headStyle" v-if="slots['logo']">
+  <div class="h-full flex">
+    <div class="flex flex-col relative transition-all duration-300" :style="sidebarStyle">
+      <div
+        class="border-solid border-0 border-gray-700/9"
+        :class="{
+          'border-r': showLogoRightBorder,
+          'border-b': showLogoBottomBorder
+        }"
+        :style="headStyle"
+        v-if="slots['logo']"
+      >
         <slot name="logo"></slot>
       </div>
-      <div class="bg-slate-200 flex-1" v-if="slots['sidebar']">
+      <div
+        class="flex-1 border-solid border-0 border-gray-700/9"
+        v-if="slots['sidebar']"
+        :class="{ 'border-r': showSidebarRightBorder }"
+      >
         <slot name="sidebar"></slot>
       </div>
-      <Icon class="absolute right-0 inset-y-0 my-auto" size="20" color="#8b949e">
-        <LeftCircleOutlined />
+      <Icon
+        class="absolute cursor-pointer right-0 inset-y-0 my-auto transition-all duration-300 translate-x-1/2 rounded-full bg-white"
+        :class="{ 'rotate-180': sidebarCollapseLock }"
+        size="20"
+        color="#8b949e"
+        @click="sidebarCollapseHandler(!sidebarCollapseLock)"
+      >
+        <LeftCircleTwotone />
       </Icon>
     </div>
-    <div class="flex-1 bg-slate-400 flex flex-col">
-      <div class="bg-slate-200" :style="headStyle" v-if="slots['head']">
+    <div class="flex-1 flex flex-col">
+      <div
+        class="border-solid border-0 border-gray-700/9"
+        :class="{
+          'border-b border-r-gray-700/9': showheadBottomBorder
+        }"
+        :style="headStyle"
+        v-if="slots['head']"
+      >
         <slot name="head"></slot>
       </div>
-      <div class="bg-slate-100 flex-1" v-if="slots['main']"><slot name="main"></slot></div>
-      <div class="bg-slate-600" :style="footStyle" v-if="slots['foot']">
+      <div class="flex-1" v-if="slots['main']"><slot name="main"></slot></div>
+      <div
+        class="border-solid border-0 border-gray-700/9"
+        :class="{
+          'border-t': showFootTopBorder
+        }"
+        :style="footStyle"
+        v-if="slots['foot']"
+      >
         <slot name="foot"></slot>
       </div>
     </div>
@@ -35,40 +67,31 @@
 
 <script setup lang="ts">
 import { computed, ref, useSlots } from 'vue'
-import { LeftCircleOutlined } from '@vicons/antd'
+import { LeftCircleTwotone } from '@vicons/antd'
 import { Icon } from '@vicons/utils'
+
+const emit = defineEmits(['onSidebarCollapseChange'])
 // 组件初始化，设置默认的logo和head为同样的高度60px，foot高度40px，sidebar宽度200px
 // 侧边栏可收起，设置收起时的宽度
-const props = defineProps({
+const props = withDefaults(defineProps<BaseLayout.Props>(), {
   // 侧边栏宽度
-  sidebarWidth: {
-    type: String,
-    default: '200px'
-  },
+  sidebarWidth: '200px',
   // 收起宽度
-  sidebarCollapseWidth: {
-    type: String,
-    default: '60px'
-  },
+  sidebarCollapseWidth: '60px',
   // logo和head的高度
-  headHeight: {
-    type: String,
-    default: '60px'
-  },
+  headHeight: '60px',
   // foot的高度
-  footHeight: {
-    type: String,
-    default: '40px'
-  }
+  footHeight: '40px',
+  showSidebarRightBorder: true,
+  showLogoBottomBorder: true,
+  showLogoRightBorder: true,
+  showheadBottomBorder: true,
+  showFootTopBorder: true
 })
 // 获取 setupContext.slots
 const slots = useSlots()
 
-const sidebarStyle = computed(() => {
-  return {
-    width: props.sidebarWidth
-  }
-})
+const sidebarStyle = ref({ width: props.sidebarWidth })
 const headStyle = computed(() => {
   return {
     height: props.headHeight
@@ -79,6 +102,17 @@ const footStyle = computed(() => {
     height: props.footHeight
   }
 })
+// 侧边栏收起按钮
+let sidebarCollapseLock = ref(false)
+const sidebarCollapseHandler = (isCollapse: boolean) => {
+  sidebarCollapseLock.value = isCollapse
+  if (sidebarCollapseLock.value) {
+    sidebarStyle.value.width = props.sidebarCollapseWidth
+  } else {
+    sidebarStyle.value.width = props.sidebarWidth
+  }
+  emit('onSidebarCollapseChange', isCollapse)
+}
 </script>
 
 <style scoped></style>
---
+++ packages/components/src/typings/layout.d.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: whr2349 378237242@QQ.com
+ * @Date: 2023-09-05 16:18:06
+ * @LastEditors: whr2349 378237242@QQ.com
+ * @LastEditTime: 2023-09-06 08:53:27
+ * @FilePath: \components\src\typings\layout.d.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+declare namespace BaseLayout {
+  interface Props extends Partial {
+    sidebarWidth: string
+    sidebarCollapseWidth: string
+    headHeight: string
+    footHeight: string
+    showSidebarRightBorder: boolean
+    showLogoBottomBorder: boolean
+    showLogoRightBorder: boolean
+    showheadBottomBorder: boolean
+    showFootTopBorder: boolean
+  }
+}
+
+
